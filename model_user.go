e8152af4001d8bf7cc30d976a45496ab133e1842
xuhui1 <xuhui1@hujiang.com>
2020-06-22 17:29:38 +0800

    fix sdk page args error

--- model_user.go
+++ model_user.go
@@ -6,7 +6,7 @@ import "time"
 type ProfileResponse struct {
 	UserID      uint64    `json:"user_id"`
 	Name        string    `json:"name,omitempty"`
-	KycLevel    string    `json:"kyc_level,omitempty"`
+	KycLevel    int       `json:"kyc_level,omitempty"`
 	KycStatus   string    `json:"kyc_status,omitempty"`
 	KycError    string    `json:"kyc_error,omitempty"`
 	IDDigest    string    `json:"id_digest,omitempty"`
